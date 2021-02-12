4d83463ff7f07eebc90be2a68f0eb065696aead5
fridawalegard <frida.walegard@iths.se>
2021-02-12 12:03:30 +0100

    skapat data för kaffesorter

--- airbean-project/src/store/index.js
+++ airbean-project/src/store/index.js
@@ -4,7 +4,48 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
+  
   state: {
+
+      articles: [
+        {
+          id:1,
+          name: "Bryggkaffe",
+          description: "Bryggd på månadens bönor",
+          price: "49"
+        },
+        {
+          id:2,
+          name: "Caffè Doppio",
+          description: "Bryggd på månadens bönor",
+          price: "49"
+        },   
+        {
+          id:3,
+          name: "Cappuccino",
+          description: "Bryggd på månadens bönor",
+          price: "49"
+        },     
+        {
+          id:4,
+          name: "Latte Macchiato",
+          description: "Bryggd på månadens bönor",
+          price: "49"
+        },     
+        {
+          id:5,
+          name: "Kaffe Latte",
+          description: "Bryggd på månadens bönor",
+          price: "49"
+        },     
+        {
+          id:6,
+          name: "Cortado",
+          description: "Bryggd på månadens bönor",
+          price: "39"
+        }
+      ]
   },
   mutations: {
   },
