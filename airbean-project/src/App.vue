e7dff4c5e2d6fc312fb3f57e8c139fd315a9ad88
annikakubaj <annika.kubaj@iths.se>
2021-02-12 13:12:41 +0100

    uppdaterat vyer

--- airbean-project/src/App.vue
+++ airbean-project/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Landing</router-link> |
+      <router-link to="/MinProfil">Min profil</router-link>
     </div>
     <router-view/>
   </div>
