2720758a93e18d5b50e2e226105b123a1218fafb
annikakubaj <annika.kubaj@iths.se>
2021-02-15 15:14:02 +0100

    lagt in logo samt bilder i assets samt påbörjat landing page

--- airbean-project/src/views/Landing.vue
+++ airbean-project/src/views/Landing.vue
@@ -1,20 +1,44 @@
 <template>
+<main>
+<section class="container">
 
-  <div class="landing">
+<div> 
+   <h1> AIR BEAN </h1>
+     <p> DRONEDELIVERED COFFEE </p>
+     </div>
 
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+</section>
+
+</main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Landing',
-  components: {
-    HelloWorld
-  }
-}
+// export default {
+//   name: 'Landing',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
+
+<style scoped>
+
+main {
+  display: flex;
+  justify-content: center;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  height: 700px;
+  width: 500px;
+  background-color: #0E927D;
+
+}
+
+</style>
