08b452ed43e292ea5907e6daeb4606f5decb417a
Louie <louie.bergendal@gmail.com>
2021-02-15 16:58:10 +0100

    Lagt till MenuList som komponent

---
+++ airbean-project/src/components/MenuList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+
+    <h1>MENY</h1>
+
+    <div v-for="art in articles" :key="art.id">
+      <div v-on:click="doStuff">
+        <h2>{{art.name}}</h2>
+        <p>Beskrivning: {{art.description}}</p>
+        <p>Pris: {{art.price}}</p>
+      </div>
+    </div>
+
+    <button v-on:click="increment">INCREMENT BUTTON</button>
+  </div>
+</template>
+
+<script>
+export default {
+    
+  computed: {
+
+    articles: function(){ // Hämtar första kaffesorten från global space.
+      return this.$store.state.articles;
+    }
+  },
+  methods: {
+
+    increment: function() {
+      this.$store.commit('increment', 10) // Kallar på "Increment" i Store
+      console.log(this.$store.state.articles)
+    },
+    doStuff: function() {
+      console.log("HEJ") // Lirar
+      console.log(this.atricle.id) // Lirar inte
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
