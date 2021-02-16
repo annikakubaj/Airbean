009932fb86aab6890129951e4450b0198cda732a
annikakubaj <annika.kubaj@iths.se>
2021-02-16 15:51:17 +0100

    landing nästan klar, justera loggan återstår

--- airbean-project/src/views/Landing.vue
+++ airbean-project/src/views/Landing.vue
@@ -1,14 +1,15 @@
 <template>
-<main>
+
 <section class="container">
 
-<div class="logo"> <img src="../assets/landing logo.svg"/> </div>
-<div class="left"> <img src="../assets/landing left.svg"/> </div>
-<div class="right"> <img src="../assets/landing right.svg"/> </div>
+
+<div class="left"><img src="../assets/landing left.svg"/> </div>
+<div class="logo"><img src="../assets/landing logo.svg"/> </div> 
+<div class="right"><img src="../assets/landing right.svg"/> </div>
 
 </section>
 
-</main>
+
 </template>
 
 <script>
@@ -25,28 +26,31 @@
 
 <style scoped>
 
-main {
-  display: flex;
-  justify-content: center;
-}
+
+
 
 .container {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 1fr;
-  width: 570px;
-  background-color: #0E927D;
-
+   display: flex;
+    justify-content: center;
+    height:665px;
+    width: 375px;
+    background-color: #0E927D;
+    
 }
+
 
 .logo {
-  grid-column: 2;
-  grid-row: 1;
-  margin-top: 100%;
-  ;
+  margin-top: 250px;
+  align-self: baseline;
   
+}
 
+.left {
+margin-right: -193px;
 
 }
+
+
+
 
 </style>
