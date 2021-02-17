03a7a90fe6c3d05df79e132a28aff7f55f081300
annikakubaj <annika.kubaj@iths.se>
2021-02-17 10:21:46 +0100

    skapat en user profile-komponent & vy

---
+++ airbean-project/src/views/UserProfile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="userProfile">
+
+      <img src="../assets/header.svg" alt="header">
+      <UserProfile/>
+
+
+      </div>
+</template>
+
+<script>
+import UserProfile from '../components/UserProfile.vue'
+
+export default {
+    components: {
+        UserProfile,
+    }
+
+}
+</script>
+
+<style>
+
+div {
+    background-color:rgba(47, 41, 38, 1);
+  }
+
+</style>
