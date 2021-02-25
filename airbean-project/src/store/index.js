8abfcebdc97564bc8a172a691c760d9aa839a115
fridawalegard <frida.walegard@iths.se>
2021-02-25 15:39:09 +0100

    lagt till data i Store

--- airbean-project/src/store/index.js
+++ airbean-project/src/store/index.js
@@ -13,42 +13,54 @@ export default new Vuex.Store({
           name: "Bryggkaffe",
           description: "Bryggd på månadens bönor",
           price: "49",
-          amount: 0
+          amount: 0,
+          info: "Lättrostat kaffe som dricks bäst i sin rena form. Kaffet är mindre intensivt i smaken men har ett bredare spektrum av smaker. Dessutom har lättrostat kaffe en högre koffeinhalt eftersom koffeinet försvinner ju kraftigare rostning.",
+          rost: "Lättrost"
         },
         {
           id: 2,
           name: "Caffè Doppio",
           description: "Bryggd på andra månadens bönor",
           price: "49",
-          amount: 0
+          amount: 0,
+          info: "En dubbel och fyllig espresso med tjock och härlig crema",
+          rost: "Mörkrost"
         },   
         {
           id: 3,
           name: "Cappuccino",
           description: "Bryggd på tredje månadens bönor",
           price: "49",
-          amount: 0
+          amount: 0,
+          info: "Vår cappuccino serveras med en enkel men stark espresso som bas tillsammans med lika delar ångad mjölk och mjölkskum.",
+          rost: "Mörkrost"
         },     
         {
           id: 4,
           name: "Latte Macchiato",
           description: "Bryggd på fjärde månadens bönor",
           price: "49",
-          amount: 0
+          amount: 0,
+          info: "Vår Macchiato består av 4/5 skummad mjölk toppad med en stark espresso.",
+          rost: "Mellanrost"
         },     
         {
           id: 5,
           name: "Kaffe Latte",
           description: "Bryggd på femte månadens bönor",
           price: "49",
-          amount: 0
+          amount: 0,
+          info: "Vår Kaffe Latte är gjord på en tredjedel espresso, två tredjedelar ångad mjölk med ett tunt lager av mjölkskum på toppen.",
+          rost: "Mellanrost"
         },     
         {
           id: 6,
           name: "Cortado",
           description: "Bryggd på sjätte månadens bönor",
           price: "39", 
-          amount: 0
+          amount: 0,
+          info: "Består av lika delar espresso och ångad mjölk.",
+          rost: "Mörkrost"
         }
       ],
       cart: [
